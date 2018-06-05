d74f447ce66be49774881b798c3ce760434bf10e
kees <cheuperman@gmail.com>
2018-06-05 21:50:29 +0100

    Refactor to case expression

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,8 @@
 def fizzbuzz(number)
-  if number % 15 == 0
-    'fizzbuzz'
-  elsif number % 5 == 0
-    'buzz'
-  elsif number % 3 == 0
-    'fizz'
+  case
+  when number % 15 == 0 then 'fizzbuzz'
+  when number % 5 == 0 then 'buzz'
+  when number % 3 == 0 then 'fizz'
   else number
   end
 end
