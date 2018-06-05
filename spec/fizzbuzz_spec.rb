dc8b4e808692d61db38a1ef76c8db9e3eada5331
kees <cheuperman@gmail.com>
2018-06-05 21:47:24 +0100

    Add fizzbuzz method to fulfil rspec

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,15 +4,12 @@ describe 'Fizzbuzz' do
   it 'returns 1 if number is 1' do
     expect(fizzbuzz(1)).to eq 1
   end
-
   it 'returns "fizz" if number is divisible by 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
-
   it 'returns "buzz" if number is divisible by 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
-
   it 'returns "fizzbuzz" if number is divisible by 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
